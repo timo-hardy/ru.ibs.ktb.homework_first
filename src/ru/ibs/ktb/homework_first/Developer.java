9a29f161bbf35f3382de7a8c4f533d165aace89c
timurio <timos.gooogle@gmail.com>
2021-10-23 18:51:20 +0500

    Added fields, constructors and getters and setters in Developer.java

--- src/ru/ibs/ktb/homework_first/Developer.java
+++ src/ru/ibs/ktb/homework_first/Developer.java
@@ -1,4 +1,29 @@
 package ru.ibs.ktb.homework_first;
 
-public class Developer {
+public class Developer extends Employee{
+    private String developmentLanguage;
+    private int developmentExperience;
+
+    public Developer(String name, String surname, int age, String gender, String mail,
+                     String developmentLanguage, int developmentExperience) {
+        super(name, surname, age, gender, mail);
+        this.developmentLanguage = developmentLanguage;
+        this.developmentExperience = developmentExperience;
+    }
+
+    public String getDevelopmentLanguage() {
+        return developmentLanguage;
+    }
+
+    public void setDevelopmentLanguage(String developmentLanguage) {
+        this.developmentLanguage = developmentLanguage;
+    }
+
+    public int getDevelopmentExperience() {
+        return developmentExperience;
+    }
+
+    public void setDevelopmentExperience(int developmentExperience) {
+        this.developmentExperience = developmentExperience;
+    }
 }
