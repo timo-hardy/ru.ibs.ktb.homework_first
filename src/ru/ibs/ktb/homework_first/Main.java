20bbdcc90ac981c83776fccd325d3ecbf7922827
timurio <timos.gooogle@gmail.com>
2021-10-26 16:36:53 +0500

    Homework2

--- src/ru/ibs/ktb/homework_first/Main.java
+++ src/ru/ibs/ktb/homework_first/Main.java
@@ -5,6 +5,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
+import java.util.stream.DoubleStream;
 
 public class Main {
     public static void main(String[] args) {
@@ -68,14 +69,39 @@ public class Main {
                 )
         );
 
-
-        List<Employee> employeesList = new ArrayList<>(employeeSet) //
+        // фильтрует по возрасту
+        List<Employee> employeesListAge = new ArrayList<>(employeeSet)
                 .stream()
                 .filter(e -> e.getAge() >= 25)
                 .collect(Collectors.toCollection(ArrayList::new));
-        System.out.println(employeesList);
+        System.out.println(employeesListAge);
+
+        // фильтрует по имени
+        List<Employee> employeesListName = new ArrayList<>(employeeSet)
+                .stream()
+                .filter(e -> e.getName() == "Katya")
+                .collect(Collectors.toCollection(ArrayList::new));
+        System.out.println(employeesListName);
+
+        // фильтрует по классу Developer
+        List<Employee> employeesListDeveloper = new ArrayList<>(employeeSet)
+                .stream()
+                .filter(e -> e instanceof  Developer)
+                .collect(Collectors.toCollection(ArrayList::new));
+        System.out.println(employeesListDeveloper);
+
+        //Поток поля имени
+        employeeSet.stream()
+                .map(employee -> employee.getName())
+                .forEach(System.out::println);
+
+        // Поток из поля возраста
+        employeeSet.stream()
+                .map(employee -> employee.getAge())
+                .forEach(System.out::println);
 
 
+        //Анонимный класс
         Employee security = new Employee("Vladimir", "Popov", 23, "male", "Popov@mail.ru") {
 
             private boolean weapon;
---
+++ src/ru/ibs/ktb/homework_second/Main.java
@@ -0,0 +1,4 @@
+package ru.ibs.ktb.homework_second;
+
+public class Main {
+}
